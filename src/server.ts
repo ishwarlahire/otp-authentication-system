24721ee93fd990b0cbd9637185da94fba47dac42
ishwarlahire <ishwarlahire2004@gmail.com>
2026-03-06 11:37:42 +0530

    frontend entigration changes import npm install @fastify/cors@8

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,22 @@ import Fastify from 'fastify';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes';
 import jwtPlugin from '@fastify/jwt';
+import cors from "@fastify/cors";
 
 dotenv.config();
 
-
 const app = Fastify({ logger: true });
-
-app.register(jwtPlugin, { secret: process.env.JWT_SECRET! });
-
-app.register(authRoutes, { prefix: '/api/auth' });
 
 const start = async () => {
   try {
+    await app.register(cors, {
+      origin: true
+    });
+
+    app.register(jwtPlugin, { secret: process.env.JWT_SECRET! });
+
+    app.register(authRoutes, { prefix: '/api/auth' });
+
     await app.listen({ port: Number(process.env.PORT) || 5000 });
     console.log('Server running');
   } catch (err) {
